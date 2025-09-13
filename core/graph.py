e0cb1200f4c9031b924d6054227f9c6e8b5b69ca
facelesshadow <aryanyadavsh369@gmail.com>
2025-09-13 21:22:19 +0530

    ...

---
+++ core/graph.py
@@ -0,0 +1,104 @@
+from typing import List
+from typing_extensions import TypedDict
+from pydantic import BaseModel, Field
+
+class Analyst(BaseModel):
+    affiliation: str = Field(
+        description="Primary affiliation of the analyst.",
+    )
+    name: str = Field(
+        description="Name of the analyst."
+    )
+    role: str = Field(
+        description="Role of the analyst in the context of the topic.",
+    )
+    description: str = Field(
+        description="Description of the analyst focus, concerns, and motives.",
+    )
+    @property
+    def persona(self) -> str:
+        return f"Name: {self.name}\nRole: {self.role}\nAffiliation: {self.affiliation}\nDescription: {self.description}\n"
+
+class Perspectives(BaseModel):
+    analysts: List[Analyst] = Field(
+        description="Comprehensive list of analysts with their roles and affiliations.",
+    )
+
+class GenerateAnalystsState(TypedDict):
+    topic: str # Research topic
+    max_analysts: int # Number of analysts
+    human_analyst_feedback: str # Human feedback
+    analysts: List[Analyst] # Analyst asking questions
+
+    
+from IPython.display import Image, display
+from langgraph.graph import START, END, StateGraph
+from langgraph.checkpoint.memory import MemorySaver
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+
+analyst_instructions="""You are tasked with creating a set of AI analyst personas. Follow these instructions carefully:
+
+1. First, review the research topic:
+{topic}
+        
+2. Examine any editorial feedback that has been optionally provided to guide creation of the analysts: 
+        
+{human_analyst_feedback}
+    
+3. Determine the most interesting themes based upon documents and / or feedback above.
+                    
+4. Pick the top {max_analysts} themes.
+
+5. Assign one analyst to each theme."""
+
+def create_analysts(state: GenerateAnalystsState):
+    
+    """ Create analysts """
+    
+    topic=state['topic']
+    max_analysts=state['max_analysts']
+    human_analyst_feedback=state.get('human_analyst_feedback', '')
+        
+    # Enforce structured output
+    structured_llm = llm.with_structured_output(Perspectives)
+
+    # System message
+    system_message = analyst_instructions.format(topic=topic,
+                                                            human_analyst_feedback=human_analyst_feedback, 
+                                                            max_analysts=max_analysts)
+
+    # Generate question 
+    analysts = structured_llm.invoke([SystemMessage(content=system_message)]+[HumanMessage(content="Generate the set of analysts.")])
+    
+    # Write the list of analysis to state
+    return {"analysts": analysts.analysts}
+
+def human_feedback(state: GenerateAnalystsState):
+    """ No-op node that should be interrupted on """
+    pass
+
+def should_continue(state: GenerateAnalystsState):
+    """ Return the next node to execute """
+
+    # Check if human feedback
+    human_analyst_feedback=state.get('human_analyst_feedback', None)
+    if human_analyst_feedback:
+        return "create_analysts"
+    
+    # Otherwise end
+    return END
+
+# Add nodes and edges 
+builder = StateGraph(GenerateAnalystsState)
+builder.add_node("create_analysts", create_analysts)
+builder.add_node("human_feedback", human_feedback)
+builder.add_edge(START, "create_analysts")
+builder.add_edge("create_analysts", "human_feedback")
+builder.add_conditional_edges("human_feedback", should_continue, ["create_analysts", END])
+
+# Compile
+memory = MemorySaver()
+graph = builder.compile(interrupt_before=['human_feedback'], checkpointer=memory)
+
+# View
+display(Image(graph.get_graph(xray=1).draw_mermaid_png()))
